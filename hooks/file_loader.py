0316c63a82137287553e08ff1d292d3082965a2e
Chandrakanth S <45536998+chandruvfx@users.noreply.github.com>
2021-12-02 01:51:28 +0530

    Update file_loader.py

--- hooks/file_loader.py
+++ hooks/file_loader.py
@@ -28,7 +28,7 @@ abc_destination_path = '/tmp/3de_alembic_import.py'
 
 scene_file = tde4.getProjectPath()
 
-if scene_file is not None and scene_file.startswith('/Shares/T/studio/projects'):
+if scene_file is not None and scene_file.startswith('/mnt/studio/mmprjs'):
     
     if '3de4' not in os.path.dirname(scene_file):
         scene_file_prj_folder_path =os.path.join(
